de1c707fb5d1bb125d6f910ff60637e029dbabd9
Yasir Hassan <yasirhassankhilji@gmail.com>
2020-10-22 13:36:57 +0500

    :sparkle: Cellular Network Done

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,32 @@
  */
 
 #include "mbed.h"
+#include "mbed-trace/mbed_trace.h"
 #include "Utils/Utils.h"
+#include "Network/Network.h"
+
+NetworkInterface* net;
+Network nwk;
 
 int main() {
     LOG_DEBUG("Firmware Version: %s", VERSION);
     LOG_DEBUG("Built on: %s %s", __DATE__, __TIME__);
+
+    mbed_trace_init();
+
+    net = nwk.begin();
+
+    if (nwk.connect() != NSAPI_ERROR_OK) {
+        LOG_ERROR("Error establishing connection");
+        delete net;
+        mbed_trace_free();
+
+        return -1;
+    } else {
+        LOG_INFO("Connected");
+    }
+
+    nwk.disconnect();
 
     return 0;
 }
